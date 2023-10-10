03269cf6b69bc24c0ea1ceef342a0da332abff21
AnitaNamjoon <anitachepkemoi1@gmail.com>
2023-10-10 15:43:07 +0300

    New commit

---
+++ app/models.py
@@ -0,0 +1,64 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Power(db.Model):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+
+    # Define the relationship from Power to HeroPower
+    hero_powers = db.relationship('HeroPower', back_populates='power')
+
+    # Define the relationship from Power to Hero through HeroPower
+    heroes = db.relationship(
+        'Hero', secondary='hero_powers', back_populates='powers')
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if not description or len(description) < 20:
+            raise ValueError("Description must be at least 20 characters long")
+        return description
+
+
+class Hero(db.Model):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    super_name = db.Column(db.String(255), nullable=False)
+
+    # Define the relationship from Hero to HeroPower
+    hero_powers = db.relationship('HeroPower', back_populates='hero')
+
+    # Define the relationship from Hero to Power through HeroPower
+    powers = db.relationship(
+        'Power', secondary='hero_powers', back_populates='heroes')
+
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey(
+        'powers.id'), nullable=False)
+    strength = db.Column(db.String(255))
+
+    # Define the relationship from HeroPower to Hero
+    hero = db.relationship('Hero', back_populates='hero_powers')
+
+    # Define the relationship from HeroPower to Power
+    power = db.relationship('Power', back_populates='hero_powers')
+
+    @validates('strength')
+    def validate_strength(self, key, strength):
+        if strength not in ['Strong', 'Weak', 'Average']:
+            raise ValueError(
+                "Strength must be one of 'Strong', 'Weak', or 'Average'")
+        return strength
