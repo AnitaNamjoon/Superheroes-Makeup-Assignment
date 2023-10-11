652a68da848c09cf1d2e0a301cafe8f31b576d6e
AnitaNamjoon <anitachepkemoi1@gmail.com>
2023-10-11 19:10:38 +0300

    New commit

--- client/src/components/HeroForm.js
+++ client/src/components/HeroForm.js
@@ -6,17 +6,19 @@ import axios from 'axios';
 const HeroForm = () => {
   const initialValues = {
     name: '',
+    super_name: '',
     power: '',
   };
 
   const validationSchema = Yup.object({
     name: Yup.string().required('Name is required'),
+    super_name: Yup.string().required('Super Name is required'),
     power: Yup.string().required('Power is required'),
   });
 
   const handleSubmit = async (values) => {
     try {
-      const response = await axios.post('https://your-api-url.com/heroes', values);
+      const response = await axios.post('/heroes', values);
       console.log('Hero added successfully:', response.data);
       // You can handle success or navigation here
     } catch (error) {
@@ -37,6 +39,12 @@ const HeroForm = () => {
             <label htmlFor="name">Name:</label>
             <Field type="text" id="name" name="name" />
             <ErrorMessage name="name" component="div" />
+          </div>
+
+          <div>
+            <label htmlFor="super_name">Super Name:</label>
+            <Field type="text" id="super_name" name="super_name" />
+            <ErrorMessage name="super_name" component="div" />
           </div>
 
           <div>
