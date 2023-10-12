b89294237a2fec3c721e3b3eefc18e89ac27a197
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-10-12 20:47:54 +0300

    Update HeroDetails.js

--- client/src/components/HeroDetails.js
+++ client/src/components/HeroDetails.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import './HeroDetails.css';
+import 'animate.css';
 
 const HeroDetails = ({ hero }) => {
   if (!hero) {
     return (
-      <div>
-        <h2>Hero Details</h2>
-        <p>No hero selected</p>
+      <div className="hero-details-container">
+        <h2 className="hero-title">Superheroes</h2> {/* Change this line */}
+        <p className="hero-description">No hero selected</p>
+        <p className="hero-description animate__animated animate__fadeIn"></p>
       </div>
     );
   }
 
   return (
-    <div>
-      <h2>Hero Details</h2>
-      <p>Name: {hero.name}</p>
-      {hero.powers ? (
-        <div>
-          <p>Power:</p>
-          <ul>
-            {hero.powers.map((power) => (
-              <li key={power.id}>{power.name}</li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>No powers available</p>
-      )}
+    <div className="hero-details-container">
+      <img className="hero-image" src={hero.image} alt={hero.name} />
+      <h2 className="hero-name">{hero.name}</h2>
+      <div className="hero-description">
+        <p>Name: {hero.name}</p>
+        {hero.powers ? (
+          <div>
+            <p>Power:</p>
+            <ul>
+              {hero.powers.map((power) => (
+                <li key={power.id}>{power.name}</li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>No powers available</p>
+        )}
+      </div>
     </div>
   );
 };
 
 export default HeroDetails;
+
