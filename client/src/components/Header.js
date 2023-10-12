4eb6447f865b4e9eca433dff4f810815814733df
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-10-12 20:46:30 +0300

    Add files via upload

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'; // Import the CSS file
+
+const Header = () => {
+  return (
+    <div className="header margin-bottom"> {/* Apply the margin-bottom class */}
+      <h2>Superheroes</h2> 
+    </div>
+  );
+};
+
+export default Header;
