22842ceb49461ad8910a513db83cc74f1e78b063
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-10-12 23:54:32 +0300

    Update HeroForm.js

--- client/src/components/HeroForm.js
+++ client/src/components/HeroForm.js
@@ -1,69 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-function HeroForm() {
-  const [formData, setFormData] = useState({
-    strength: 5,
-    power_id: 2,
-    hero_id: 1,
-  });
-
-  const handleStrengthChange = (event) => {
-    const strength = Number(event.target.value);
-    setFormData({ ...formData, strength });
-  };
-
-  const handlePowerIdChange = (event) => {
-    const power_id = Number(event.target.value);
-    setFormData({ ...formData, power_id });
-  };
-
-  const handleHeroIdChange = (event) => {
-    const hero_id = Number(event.target.value);
-    setFormData({ ...formData, hero_id });
-  };
-
-  const handleSubmit = () => {
-    // Send the formData to the Flask backend
-    fetch('http://127.0.0.1:5555/hero_powers', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Response from server:', data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
-
+function HeroForm({ availablePowers, onSubmit }) {
   return (
     <div>
-      <label>Strength: </label>
-      <input
-        type="number"
-        value={formData.strength}
-        onChange={handleStrengthChange}
-      />
-      <br />
-      <label>Power ID: </label>
-      <input
-        type="number"
-        value={formData.power_id}
-        onChange={handlePowerIdChange}
-      />
-      <br />
-      <label>Hero ID: </label>
-      <input
-        type="number"
-        value={formData.hero_id}
-        onChange={handleHeroIdChange}
-      />
-      <br />
-      <button onClick={handleSubmit}>Submit</button>
+      <h2>Create a New Hero</h2>
+      <Formik
+        initialValues={{ name: '', superpower: '' }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.name) {
+            errors.name = 'Name is required';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { resetForm }) => {
+          onSubmit(values); // Call createHero with form values
+          resetForm(); // Reset the form after submission
+        }}
+      >
+        <Form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <Field type="text" id="name" name="name" />
+            <ErrorMessage name="name" component="div" className="error" />
+          </div>
+
+          <div>
+            <label htmlFor="superpower">Superpower</label>
+            <Field as="select" id="superpower" name="superpower">
+              <option value="">Select a superpower</option>
+              {availablePowers.map((power, index) => (
+                <option key={index} value={power}>
+                  {power}
+                </option>
+              ))}
+            </Field>
+          </div>
+
+          <div>
+            <button type="submit">Create Hero</button>
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 }
